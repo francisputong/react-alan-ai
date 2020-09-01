9a9c1afba91eb1d2e58b81a886dbcb1dd41319d1
francisputong <francisputong01@gmail.com>
2020-09-01 22:00:34 +0800

    integrated alan ai

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import { useState } from "react";
 
-function App() {
+import NewsCards from "./components/NewsCards/NewsCards";
+
+const alanKey =
+  "564e5bef451cbc183e54a86bd49bb5502e956eca572e1d8b807a3e2338fdd0dc/stage";
+
+const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
+
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadlines") {
+          setNewsArticles(articles);
+        }
+      },
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Alan AI News</h1>
+      <NewsCards articles={newsArticles} />
     </div>
   );
-}
+};
 
 export default App;
